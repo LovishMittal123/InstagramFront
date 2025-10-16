f724183d2122019f23caf5931530232ab78e8ca1
lovish <lovish0429.be20@chitkara.edu.in>
2025-10-16 16:07:16 +0530

    base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
-// export const BASE_URL= 
+export const BASE_URL = process.env.REACT_APP_API_URL;
+// export const BASE_URL="http://localhost:5000" 
 
