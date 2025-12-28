b98e83bf5462e22e0a5a7570e090a32060fc3645
lovish <lovish0429.be20@chitkara.edu.in>
2025-12-28 18:39:49 +0530

    new updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL ="https://instagram-front-geuu.vercel.app/";
+export const BASE_URL ="https://instagram-front-plum.vercel.app";
 // export const BASE_URL = "http://localhost:5000";
 
 
